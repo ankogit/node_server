de7060c942c9bba8271b230634ae3bebf7947698
Anatoliy Antoneko <ya.tolik-a@yandex.ru>
2017-11-15 01:06:16 +0300

    js sctipts placeholded

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,3 +1,18 @@
-$(function() {
-    // body...
-})
+      $( "form" ).delegate( "input", "focus", function() {
+        var elem = $( this );
+        setTimeout(function() {
+          if(elem.data('placeholder')) {
+            //alert(elem.data('placeholder'));
+            var placeholder_text = elem.data('placeholder');
+            elem.toggleClass( "focused", elem.is( ":focus" ) );
+            $( '<span class="place">'+placeholder_text+'</span>' ).insertAfter(elem);
+          }
+        }, 0 );
+      });
+      $( "form" ).delegate( "input", "blur", function() {
+        var elem = $( this );
+        setTimeout(function() {
+          $( ".place" ).remove();
+        }, 0 );
+      });
+     
